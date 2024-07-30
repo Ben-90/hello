e06d58c2a679f2a980b762d6444b0299f6b314a7
Ben-90 <mohamed.benomar@ucad.edu.sn>
2024-07-30 15:57:59 +0000

    the season file

---
+++ seasons.py
@@ -0,0 +1,20 @@
+from datetime import date
+import sys
+class Seasons:
+    def init():
+        ...
+    def __repr__(self) -> str:
+        ...
+
+
+
+def get_birth():
+    birth = input("enter your birth : ")
+    
+
+def main():
+    a = get_birth()
+    print(a)
+
+if __name__ == main():
+    main()
