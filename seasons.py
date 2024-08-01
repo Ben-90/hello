c92aa514fe5f663624d7a1bab8fb6cbbfb65cfb7
Ben-90 <mohamed.benomar@ucad.edu.sn>
2024-08-01 17:44:53 +0000

    this is some changes of seasons file

--- seasons.py
+++ seasons.py
@@ -12,7 +12,7 @@ def get_birth():
     try:
         birth = input("Enter your birthdate (YYYY-MM-DD): ").split("-")
         date_saisie = date(int(birth[0]), int(birth[1]), int(birth[2]))
-        return (date.today - date_saisie).year
+        return (date.today().year - date_saisie.year())
     except ValueError:
         sys.exit("Invalid date")
     
