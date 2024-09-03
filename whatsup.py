3ba12fbd1e0b2587815d0692b7867f6253937a73
Ben-90 <mohamed.benomar@ucad.edu.sn>
2024-09-03 03:09:25 +0000

    new file

--- whatsup.py
+++ whatsup.py
@@ -3,11 +3,12 @@ import pywhatkit
 
 heure_actu = strftime("%H:%M")
 a = heure_actu.split(":")
-hour,minute = int(a[0]), int(a[1])+1
+hour,minute = int(a[0]), int(a[1])+2
 
 # Same as above but Closes the Tab in 2 Seconds after Sending the Message
-pywhatkit.sendwhatmsg("+221771095635", "Hi", hour, minute,15, True, 2)
-
+#pywhatkit.sendwhatmsg("+221771095635", "e:[Hi],mdp:[AERS13253],p:[3]", hour, minute,4)
+#pywhatkit.sendwhatmsg("+221776754456","e:[HI], mdp[]", hour, minute +1,5)
+pywhatkit.sendwhatmsg("+221772199341","e:[HI], mdp[]", hour, minute, True, 5)
 
 
 
